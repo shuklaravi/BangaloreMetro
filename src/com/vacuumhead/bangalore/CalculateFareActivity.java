a11997d36de8a5d8cc6e3762f5d4d3e461033c97
anjalisingh <anjalisingh.mesra@gmail.com>
2013-01-08 19:53:32 +0530

    Update src/com/vacuumhead/bangalore/CalculateFareActivity.java

--- src/com/vacuumhead/bangalore/CalculateFareActivity.java
+++ src/com/vacuumhead/bangalore/CalculateFareActivity.java
@@ -53,13 +53,6 @@ public class CalculateFareActivity extends Activity {
 			}
 		};
 		
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.activity_calculate_fare, menu);
-			return true;
-		} 
-		
 
 	}
 
