56be7ee8cc90627c50edbcf273879665e6d0ff4b
anjalisingh.mesra@gmail.com <anjalisingh.mesra@gmail.com>
2013-01-09 19:01:16 +0530

    modified

--- src/com/vacuumhead/bangalore/CalculateFareActivity.java
+++ src/com/vacuumhead/bangalore/CalculateFareActivity.java
@@ -99,8 +99,6 @@ public  class CalculateFareActivity extends Activity  {
 		}	
 
 		
-		
-<<<<<<< HEAD
 	OnClickListener mainMenuClickListener = new OnClickListener() {
 			
 			@Override
@@ -147,34 +145,3 @@ public  class CalculateFareActivity extends Activity  {
 	}
 	
 }	
-=======
-	//	AdjacencyMatrix adjMatrix;
-	  //  String s=adjMatrix.func(source,dest);	
-	
-		String func(String sourceStation, String destinationStation)
-		{
-			
-			map.put("Baiyyappanahalli",1);
-			map.put("Swami Vivekananda Road",2);
-			map.put("Indira Nagar",3);
-			map.put("Halasuru",4);
-			map.put("Trinity Circle",5);
-			map.put("M.G Road",6);
-			
-			
-			adjMatrix[1][1]="10";
-			adjMatrix[1][2]="10";
-			adjMatrix[1][3]="12";
-			adjMatrix[1][4]="12";
-			adjMatrix[1][5]="14";
-			adjMatrix[1][6]="15";
-			
-			int sourceKey=(Integer)map.get(sourceStation);
-			int destinationKey=(Integer)map.get(destinationStation);
-					
-			int count=Math.abs(destinationKey-sourceKey)+1;
-			return adjMatrix[1][count];
-		}
-
-	}
->>>>>>> 5305d55ed113ed7e7de289ccc0472a6b95b812b1
