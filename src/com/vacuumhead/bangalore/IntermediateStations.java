69cbf7809c8b9ef80de26190f5f21d48d0f335aa
Pratyush Verma <pratyush.verma@flipkart.com>
2013-01-25 18:33:42 +0530

    added intermediate station

--- src/com/vacuumhead/bangalore/IntermediateStations.java
+++ src/com/vacuumhead/bangalore/IntermediateStations.java
@@ -2,11 +2,17 @@ package com.vacuumhead.bangalore;
 
 import java.util.Vector;
 
+import com.vacuumhead.bangalore.constants.StationConstants;
 import com.vacuumhead.bangalore.utils.MetroMapData;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,14 +20,29 @@ public class IntermediateStations extends Activity {
 
 	public static final String Source="Source";
 	public static final String Dest="Destination";
-	private Vector<String> stations;
+	
 	protected void onCreate(Bundle saved)
 	{
 		super.onCreate(saved);
+		setContentView(R.layout.activity_intermediate_station); 
+		
 		Bundle extras=getIntent().getExtras();
 		String s=extras.getString(Source);
 		String d=extras.getString(Dest);
 		
-		//Toast.makeText(getBaseContext(), "Source!  "+ s+"\nDest!  "+d+dd, Toast.LENGTH_SHORT).show();
+		Vector<String> intermediateStation = MetroMapData.applyBFS(s, d);
+		LayoutInflater tableInflator = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);		
+		
+		TableLayout table = (TableLayout) findViewById(R.id.intermediateTable);
+		
+		for(int i = 0; i < intermediateStation.size(); ++i) {
+			View tableRow = tableInflator.inflate(R.layout.intermediate_each_station, null);
+			TextView name = (TextView) tableRow.findViewById(R.id.stationName);
+			name.setText(intermediateStation.get(i));
+			
+			table.addView(tableRow, i);
+			
+		}
+				
 }
 }
